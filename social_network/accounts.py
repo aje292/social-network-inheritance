21c78734d2b4f6b60c5ea4e903632a87ad31a494
aje292 <aje292@nyu.edu>
2019-05-24 20:19:24 +0000

    Created rmotrgram functionality. All tests passed.

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,22 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        tl = []
+        for u in self.following:
+            for post in u.posts:
+                tl.append(post)
+        return sorted(tl, key=lambda p:p.timestamp, reverse=False)
 
     def follow(self, other):
-        pass
+        return self.following.append(other)
